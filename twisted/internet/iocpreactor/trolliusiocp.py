243922f390f5eaeda5cabab053b40e1e22f191f4
HawkOwl <hawkowl@atleastfornow.net>
2015-09-25 14:36:52 +0800

    moar

--- twisted/internet/iocpreactor/trolliusiocp.py
+++ twisted/internet/iocpreactor/trolliusiocp.py
@@ -9,3 +9,14 @@ class CompletionPort(object):
     """
     An IOCP CompletionPort thing.
     """
+
+    def __init__(self):
+
+        self.port = _overlapped.CreateIoCompletionPort(
+            _overlapped.INVALID_HANDLE_VALUE, 0, 0, 0)
+
+
+    def getEvent(self, timeout):
+
+        status = _overlapped.GetQueuedCompletionStatus(self.port, timeout)
+        return status
