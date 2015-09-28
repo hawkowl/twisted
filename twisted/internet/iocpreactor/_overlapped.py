88bdb699455acd932aac2424f142dae0c951840b
HawkOwl <hawkowl@atleastfornow.net>
2015-09-28 11:50:38 +0800

    more

--- twisted/internet/iocpreactor/_overlapped.py
+++ twisted/internet/iocpreactor/_overlapped.py
@@ -15,6 +15,29 @@ ffi.set_source("_overlapped",
 
 ffi.cdef("""
 BOOL GetQueuedCompletionStatus(HANDLE, LPDWORD, PULONG_PTR, LPOVERLAPPED*, DWORD);
+
+typedef struct _OVERLAPPED {
+#ifdef WORDS_BIGENDIAN
+        ULONG_PTR InternalHigh;
+        ULONG_PTR Internal;
+#else
+        ULONG_PTR Internal;
+        ULONG_PTR InternalHigh;
+#endif
+        union {
+            struct {
+#ifdef WORDS_BIGENDIAN
+                DWORD OffsetHigh;
+                DWORD Offset;
+#else
+                DWORD Offset;
+                DWORD OffsetHigh;
+#endif
+            } DUMMYSTRUCTNAME;
+            PVOID Pointer;
+        } DUMMYUNIONNAME;
+         HANDLE hEvent;
+} OVERLAPPED, *LPOVERLAPPED;
 """)
 
 lib = ffi.dlopen(ctypes.util.find_library("c"))
